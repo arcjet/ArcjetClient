0d7d1f8425c7cbace4966aba9a2d658aaa1dfffe
cryptoquick <cryptoquick@gmail.com>
2018-10-13 15:23:38 -0600

    Add test.

--- src/index.test.ts
+++ src/index.test.ts
@@ -16,8 +16,13 @@ const mockFetchArrayBuffer = (status: number, value: Uint8Array) =>
     arrayBuffer: jest.fn().mockResolvedValue(value),
   }))
 
+const mockFetchResponse = (status: number) =>
+  ((global as any).fetch = jest.fn().mockResolvedValue({
+    status,
+  }))
+
 describe('Integration', () => {
-  it('can get a record from the server, sign, and verify it', async () => {
+  it('can get a record from the network, sign, and verify it', async () => {
     const fetch = mockFetchArrayBuffer(200, new Uint8Array([127, 0, 0, 1]))
     const api = new Arcjet()
     await api.generate()
@@ -27,5 +32,18 @@ describe('Integration', () => {
     )
     await record.sign()
     await record.verify()
+  })
+
+  it('can set a record from the network, and receive a record', async () => {
+    mockFetchResponse(200)
+    const api = new Arcjet()
+    await api.generate()
+    const record = await api.set(new Uint8Array([127, 0, 0, 1]), 'text/plain', {
+      test: 'test',
+    })
+    await record.verify()
+    expect(record.metadata.contentHash).toEqual(
+      '9f9ab00bda6cb55b87554fb3547dc06d58a90e7e58473fe179ab6eb95d61eb7aedb8b31c2190911554258b62ece60a8863c729a651001da45b3053907ee2ebc5',
+    )
   })
 })
